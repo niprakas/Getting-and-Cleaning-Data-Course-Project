aee03d986352e0faae4f7857819c0db8fa476a03
niprakas <nikhilprakash.nik@gmail.com>
2018-03-27 15:59:49 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -6,7 +6,7 @@
 #   run_analysis.R
 #
 # OVERVIEW
-#   Accelerometers data collected from the from the Samsung Galaxy S Phone 
+#   Accelerometers data collected from the from the wearable (Samsung Galaxy S PhoneII) 
 #   below script will perform the cleaning of the data and Build a clean data set for further processing.
 #
 # This script will perform the following steps
@@ -22,12 +22,10 @@
 # Clean up workspace
 rm(list=ls())
 
-# Load library ìreshape2î 
-
+# Load library ‚Äúreshape2‚Äù 
 library(reshape2)
 
 # Download and unzip the file.
-
 zipUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 zipFile <- "UCI HAR Dataset.zip"
 
@@ -35,7 +33,6 @@ if (!file.exists(zipFile))
 {
   download.file(zipUrl, zipFile, mode = "wb")
 }
-
 dataExt <- "UCI HAR Dataset"
 if (!file.exists(dataExt))
 {
@@ -49,23 +46,13 @@ activityTypes <- read.table(file.path(dataExt, "activity_labels.txt"))
 activityTypes[,2] <- as.character(activityTypes[,2])
 
 # Only extract data on standard deviation and mean.
-
-#featuresReq <- grep(".*mean.*|.*std.*", features[,2])
-#featuresReq.names <- features[featuresReq,2]
-#featuresReq.names = gsub('-mean', 'Mean', featuresReq.names)
-#featuresReq.names = gsub('-std', 'Std', featuresReq.names)
-#featuresReq.names <- gsub('[-()]', '', featuresReq.names)
-
-
 featuresReq <- grep(".*std.*|.*mean.*", features[,2])
 featuresReq.names <- features[featuresReq,2]
 featuresReq.names = gsub('-std', 'Std', featuresReq.names)
 featuresReq.names = gsub('-mean', 'Mean', featuresReq.names)
 featuresReq.names <- gsub('[-()]', '', featuresReq.names)
 
-
 # Load training and test data sets.
-
 trainSub <- read.table(file.path(dataExt, "train", "subject_train.txt"))
 trainX <- read.table(file.path(dataExt, "train", "X_train.txt"))[featuresReq]
 trainY <- read.table(file.path(dataExt, "train", "y_train.txt"))
@@ -75,7 +62,6 @@ testY <- read.table(file.path(dataExt, "test", "y_test.txt"))
 
 
 # Merges the training and the test sets to create one data set.
-
 trainData = cbind(trainSub,trainY,trainX)
 testData = cbind(testSub,testY,testX)
 
@@ -83,17 +69,14 @@ finalData = rbind(trainData,testData)
 colnames(finalData) = c("subjectNo", "activityTypes", featuresReq.names) 
 
 #create a factor for subject and activity type
-
 finalData$subject <- as.factor(finalData$subject)
 finalData$activity <- factor(finalData$activity, levels = activityTypes[,1], labels = activityTypes[,2])
 
 # Now using reshape library, "melt" function used so that each row have a unique id-variable combination.
 # and using "cast" function, the melted data reshape in to desired shape.
-
 finalData.melt <- melt(finalData, id = c("subject", "activity"))
 finalData.mean <- dcast(finalData.melt, subject + activity ~ variable, mean)
 
 
 # Export the tidyData set 
-
 write.table(finalData.mean, './tidyData.txt', row.names = FALSE, quote = FALSE, sep='\t')
